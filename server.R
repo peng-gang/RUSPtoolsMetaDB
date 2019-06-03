196fd894cc1bc88c19b797add49b38133f571899
gpeng <michael.gang.peng@gmail.com>
2019-06-02 22:17:08 -0400

    Show Panel

--- server.R
+++ server.R
@@ -1,16 +1,136 @@
 library(shiny)
+library(ggplot2)
+library(ggsci)
+library(ggpubr)
 
-# Define server logic required to draw a histogram
+load("data/RUSP.SN.RData")
+
+race.all <- c("Asian", "Black", "Hispanic", "White")
+
 shinyServer(function(input, output) {
   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- rnorm(1000)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, col = 'darkgray', border = 'white')
-    
-  })
+  output$plot <- renderPlot({
+    gp <- NULL
+    if(input$tabset == "show"){
+      title = NULL
+      meta <- NULL
+      if(input$tabShow=="single"){
+        idx <- which(meta.name==input$analytes) + 7
+        meta <- control.clean[,idx]
+        title =  paste0(input$analytes, " (\u03BCmol/L)")
+      } else {
+        numerator <- rep(0, nrow(control.clean))
+        denominator <- numerator
+        for(num.sel in input$numerator){
+          numerator <- numerator + control.clean[, which(meta.name==num.sel)+7]
+        }
+        for(denom.sel in input$denominator){
+          denominator <- denominator + control.clean[, which(meta.name==denom.sel) + 7]
+        }
+        denominator[denominator==0] <- 0.01
+        meta <- numerator/denominator
+        t1 <- paste(input$numerator, collapse = "+")
+        if(length(input$numerator) > 1){
+          t1 <- paste0("(", t1, ")")
+        }
+        t2 <- paste(input$denominator, collapse = "+")
+        if(length(input$denominator) > 1){
+          t2 <- paste0("(", t2, ")")
+        }
+        title = paste0(t1, "/", t2)
+      }
+      
+      idx.sel <- rep(TRUE, nrow(control.clean))
+      
+      if(as.numeric(input$race) > 1){
+        idx.sel <- idx.sel & control.clean$race == race.all[as.numeric(input$race)-1]
+      }
+      
+      if(as.numeric(input$sex) == 2){
+        idx.sel <- idx.sel & control.clean$sex == "M"
+      } else if(as.numeric(input$sex) == 3){
+        idx.sel <- idx.sel & control.clean$sex == "F"
+      }
+      
+      if(as.numeric(input$aac)==2){
+        idx.sel <- idx.sel & control.clean$Age_hr < 24
+      } else if(as.numeric(input$aac)==3){
+        idx.sel <- idx.sel & control.clean$Age_hr >= 24 & control.clean$Age_hr <=48
+      } else if(as.numeric(input$aac)==4){
+        idx.sel <- idx.sel & control.clean$Age_hr > 48
+      }
+      
+      if(as.numeric(input$tpn)==2){
+        idx.sel <- idx.sel & control.clean$TPN_HYPERAL==0
+      } else if(as.numeric(input$tpn)==3){
+        idx.sel <- idx.sel & control.clean$TPN_HYPERAL==1
+      }
+      
+      val <- matrix(0, 5, 5)
+      num <- matrix(0, 5, 5)
+      for(i in 1:5){
+        for(j in 1:5){
+          idx.tmp <- idx.sel & idx.BW==i & idx.GA==j
+          val[i,j] <- mean(meta[idx.tmp], na.rm = TRUE)
+          num[i,j] <- sum(idx.tmp, na.rm = TRUE)
+        }
+      }
+      
+      dplot <- data.frame(
+        GA = factor(rep(c("28-36", "37-38", "39-40", "41", "42"), each = 5), 
+                    levels = c("28-36", "37-38", "39-40", "41", "42")),
+        BW = factor(rep(c("1000-2499", "2500-3000", "3001-3500", "3501-4000","4001-5000"), 5),
+                    levels =c("1000-2499", "2500-3000", "3001-3500", "3501-4000","4001-5000")),
+        val = as.vector(val),
+        text = paste0(format(round(as.vector(val), 2), nsmall = 2), "\n(n=",
+                      num, ")")
+      )
+      
+      mx <- max(as.vector(val), na.rm = TRUE)
+      mn <- min(as.vector(val), na.rm = TRUE)
+      
+      gp.heat <- ggplot(dplot, aes(GA, BW)) + 
+        geom_raster(aes(fill = val)) + 
+        geom_text(aes(label = text), size=4) + 
+        labs(x = "Gestational Age (week)", y = "Birth Weight (g)", title = title) + 
+        theme_classic() + 
+        scale_fill_gradientn(colours = c("#00A1D5FF", "#FFFFFFFF", "#B24745FF"), limits = c(mn, mx)) +
+        geom_rect(aes(xmin = 1.5, xmax = 4.5, ymin = 1.5, ymax = 4.5), size = 0.6, fill = NA, color="black") +
+        theme(legend.title = element_blank(), text = element_text(size = 18), legend.position = "right")
+      
+      
+      idx.sel.All <- idx.sel
+      idx.sel.GA <- idx.sel & idx.GA==3
+      
+      dplot <- data.frame(
+        meta = c(meta[which(idx.sel.All)], meta[which(idx.sel.GA)]),
+        BW = c(control.clean$BW[which(idx.sel.All)], 
+               control.clean$BW[which(idx.sel.GA)]),
+        GA = c(rep("All", sum(idx.sel.All, na.rm = TRUE)),
+               rep("GA: 39-40", sum(idx.sel.GA, na.rm = TRUE)))
+      )
+      
+      
+      gp.smooth <- ggplot(dplot) + 
+        geom_smooth(aes(x=BW, y=meta, color=GA, fill = GA)) +
+        labs(x="Birth Weight (g)", y=title) +
+        scale_color_jama() + 
+        theme_light() + 
+        theme(legend.title = element_blank(),
+              text = element_text(size=18),
+              legend.position = c(0.75, 0.25))
+      
+      gp <- ggarrange(
+        gp.heat, gp.smooth, NULL,
+        ncol = 1, nrow = 3)
+    } else if(input$tabset == "compare"){
+      
+    } else if(input$tabset == "about"){
+      
+    }
+    gp
+  }, width = 500, 
+  height = 1200)
+  
   
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,135 @@
 library(shiny)
 
+load("data/RUSP.SN.RData")
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
+  includeCSS("www/style.css"),
   
   # Application title
   titlePanel("RUSPtools",
              title = h1("RUSPtools: Newborn Metabolic Screening Database")),
   
-  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       tabsetPanel(
-        tabPanel("Show", fluid = TRUE),
-        tabPanel("Compare", fluid = TRUE),
-        tabPanel("About", fluid = TRUE)
-      )
+        id = "tabset",
+        tabPanel(
+          "Show", fluid = TRUE, value = "show",
+          tabsetPanel(
+            type = "tabs",
+            id = "tabShow",
+            tabPanel(
+              "Single",
+              value = "single",
+              tags$div(
+                title = "Select an anallyte to show",
+                selectInput(
+                  "analytes",
+                  label = h4("Analytes"),
+                  choices = meta.name,
+                  selected = "C3"
+                )
+              )
+            ),
+            tabPanel(
+              "Ratio",
+              value = "ratio",
+              tags$div(
+                title = "Select a numerator and a denominator",
+                selectInput(
+                  "numerator",
+                  label = h4("Numerator"),
+                  choices = meta.name,
+                  multiple = TRUE,
+                  selected = "C3"
+                ),
+                selectInput(
+                  "denominator",
+                  label = h4("Denominator"),
+                  choices = meta.name,
+                  multiple = TRUE,
+                  selected = "C2"
+                )
+              )
+            )
+          ),
+          hr(),
+          
+          tags$div(
+            title = "Specify the race/ethnicity to show",
+            selectInput(
+              "race",
+              label = h4("Race/Ethnicity"),
+              choices = list(
+                "All" = 1,
+                "Asian" = 2,
+                "Black" = 3,
+                "Hispanic" = 4,
+                "White" = 5
+              ),
+              selected = 1
+            )
+          ),
+          
+          hr(),
+          tags$div(
+            title = "Specify the sex to show",
+            selectInput(
+              "sex",
+              label = h4("Sex"),
+              choices = list(
+                "Both" = 1,
+                "Male" = 2,
+                "Female" = 3
+              ),
+              selected = 1
+            )
+          ),
+          
+          hr(),
+          tags$div(
+            title = "Select a range of age at blood collection to show",
+            selectInput(
+              "aac",
+              label = h4("Age at blood collection"),
+              choices = list(
+                "All" = 1,
+                "12-24 hours" = 2,
+                "24-48 hours" = 3,
+                "48-168 hours" = 4
+              ),
+              selected = 3
+            ),
+            
+            hr(),
+            tags$div(
+              title = "Select TPN option",
+              selectInput(
+                "tpn",
+                label = h4("Total Parenteral Nutrition"),
+                choices = list(
+                  "All" = 1,
+                  "No TPN" = 2,
+                  "TPN" = 3
+                ),
+                selected = 2,
+              )
+            )
+          )),
+        
+        tabPanel(
+          "Compare", fluid = TRUE, 
+          value = "compare"),
+        tabPanel(
+          "About", fluid = TRUE,
+          value = "about")
+      ), width = 3
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("plot")
     )
   )
 ))
